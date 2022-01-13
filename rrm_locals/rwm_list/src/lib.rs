99a98cc75499d214e8049f2709c05c5c24ceec82
AOx0 <50227494+AOx0@users.noreply.github.com>
2022-01-13 14:10:41 -0600

    xml parser

--- rrm_locals/rwm_list/src/lib.rs
+++ rrm_locals/rwm_list/src/lib.rs
@@ -1,5 +1,1 @@
 mod path_finder;
-mod xml_analyzer;
-
-pub use path_absolutize::Absolutize;
-
--- rrm_locals/rwm_list/src/xml_analyzer.rs
+++ rrm_locals/rwm_list/src/xml_analyzer.rs
@@ -1,11 +1,2 @@
-use std::path::PathBuf;
-
-#[derive(Debug)]
-struct Mod {
-    pub path: PathBuf,
-    pub name: String,
-    pub identifier: String,
-    pub version: String
-}
 
 
