7a4751e4072f092913f034833ae9f920b612f4de
AOx0 <50227494+AOx0@users.noreply.github.com>
2022-01-16 13:09:14 -0600

    rwm & rwm_list refactor
    - Changed rwm_list -> rwm_locals
    - rwm_locals now does all the parsing, leaving GameMods and GamePaths public as the main interface, wuthout handling nothing by hand.
    - rwm_xml no loger used within rwm, only in rwm_locals
    - New structs and enums to make everything more verbose
    - Etc

--- rrm_xml/rwm_xml/src/lib.rs
+++ rrm_xml/rwm_xml/src/lib.rs
@@ -1,14 +1,13 @@
 extern crate xml;
 
-use xml::reader::{EventReader, XmlEvent};
-use std::io::{BufReader, Read};
 use std::fs::File;
-
+use std::io::{BufReader, Read};
+use xml::reader::{EventReader, XmlEvent};
 
 #[derive(Debug)]
 pub struct Element {
     pub value: String,
-    pub name: String
+    pub name: String,
 }
 
 pub trait XMLFile {
@@ -19,7 +18,7 @@ impl Clone for Element {
     fn clone(&self) -> Self {
         Element {
             value: self.value.clone(),
-            name: self.name.clone()
+            name: self.name.clone(),
         }
     }
 }
@@ -29,7 +28,7 @@ impl XMLFile for File {
         let mut r = vec![];
         let mut record = Element {
             value: "".to_string(),
-            name: "".to_string()
+            name: "".to_string(),
         };
 
         let mut file = BufReader::new(self);
@@ -40,8 +39,7 @@ impl XMLFile for File {
 
         let contents = String::from_utf8(contents).unwrap();
 
-
-        let parser= EventReader::from_str(&contents);
+        let parser = EventReader::from_str(&contents);
         let mut depth = 0;
         for e in parser {
             match e {
@@ -51,7 +49,7 @@ impl XMLFile for File {
                 }
                 Ok(XmlEvent::Characters(value)) => {
                     if keys.contains(&&*record.name) && ([0, 1, 2].contains(&depth)) {
-                        record.value  = value;
+                        record.value = value;
                         r.push(record.clone());
                     }
                 }
@@ -77,7 +75,8 @@ fn test() {
     let r = file.values_of(&["version", "identifier"]);
     println!("{:?}", r);
 
-    let file = File::open("/Applications/RimWorld.app/Mods/Area_unlocker/About/Manifest.xml").unwrap();
+    let file =
+        File::open("/Applications/RimWorld.app/Mods/Area_unlocker/About/Manifest.xml").unwrap();
     let r = file.values_of(&["version", "identifier"]);
 
     println!("{:?}", r);
@@ -93,7 +92,7 @@ fn rm_bom(contents: &mut Vec<u8>) {
     let err = contents.windows(pat.len()).position(|w| w.eq(&pat));
 
     match err {
-        Some(i) =>  {
+        Some(i) => {
             for _ in 0..3 {
                 contents.remove(i);
             }
