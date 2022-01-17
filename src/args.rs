7c5cf261e98d77f4ee2914c18d0587759d93f2ce
AOx0 <50227494+AOx0@users.noreply.github.com>
2022-01-17 17:03:49 -0600

    Move Search::Local & Commands::SearchLocally args to args::Local

--- src/args.rs
+++ src/args.rs
@@ -1,10 +1,10 @@
 use crate::utils::*;
-use clap::{AppSettings, Parser, Subcommand};
+use clap::{AppSettings, Parser, Subcommand, Args};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 #[clap(global_setting(AppSettings::ArgRequiredElseHelp))]
-pub struct Args {
+pub struct App {
     #[clap(subcommand)]
     pub(crate) command: Commands,
 
@@ -48,29 +48,8 @@ pub enum Commands {
         override_usage = "rwm search local [OPTIONS] <STRING>"
     )]
     SearchLocally {
-        /// The pattern to search
-        #[clap(required = true)]
-        r#string: String,
-
-        /// Search by author(s) name(s)
-        #[clap(short, long)]
-        authors: bool,
-
-        /// Search by version
-        #[clap(short, long)]
-        version: bool,
-
-        /// Search by Steam ID
-        #[clap(short, long)]
-        steam_id: bool,
-
-        /// Search by mod name
-        #[clap(short, long)]
-        name: bool,
-
-        /// Search by all fields
-        #[clap(long, conflicts_with_all = &["authors", "version", "steam-id", "name"])]
-        all: bool,
+        #[clap(flatten)]
+        args: Local
     },
 
     #[clap(
@@ -111,34 +90,42 @@ pub enum Search {
         about = "Search for mods locally, where RimWorld is installed [with no flags searches by name]",
     )]
     Local {
-        /// The pattern to search
-        #[clap(required = true)]
-        r#string: String,
-
-        /// Search by author(s) name(s)
-        #[clap(short, long)]
-        authors: bool,
-
-        /// Search by version
-        #[clap(short, long)]
-        version: bool,
-
-        /// Search by Steam ID
-        #[clap(short, long)]
-        steam_id: bool,
-
-        /// Search by mod name
-        #[clap(short, long)]
-        name: bool,
-
-        /// Search by all fields
-        #[clap(long, conflicts_with_all = &["authors", "version", "steam-id", "name"])]
-        all: bool,
+        #[clap(flatten)]
+        args: Local
     },
 }
 
-impl Args {
-    pub fn load() -> Args {
-        Args::parse()
+#[derive(Args, Debug)]
+pub struct Local {
+    /// The pattern to search
+    #[clap(required = true)]
+    pub(crate) r#string: String,
+
+    /// Search by author(s) name(s)
+    #[clap(short, long)]
+    pub(crate) authors: bool,
+
+    /// Search by version
+    #[clap(short, long)]
+    pub(crate) version: bool,
+
+    /// Search by Steam ID
+    #[clap(short, long)]
+    pub(crate) steam_id: bool,
+
+    /// Search by mod name
+    #[clap(short, long)]
+    pub(crate) name: bool,
+
+    /// Search by all fields
+    #[clap(long, conflicts_with_all = &["authors", "version", "steam-id", "name"])]
+    pub(crate) all: bool,
+}
+
+
+impl App {
+    pub fn load() -> App {
+        App::parse()
     }
 }
+
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod utils;
 
 #[tokio::main]
 async fn main() {
-    let args = args::Args::load();
+    let args = args::App::load();
     let path = args.game_path;
 
     match args.command {
@@ -13,14 +13,9 @@ async fn main() {
 
         args::Commands::Search { command } => match command {
             args::Search::Local {
-                string: m,
-                authors,
-                version,
-                steam_id,
-                name,
-                all,
+                 args,
             } => {
-                search::search_locally(&path, &m, authors, version, steam_id, name, all);
+                search::search_locally(&path, &args.string, args.authors, args.version, args.steam_id, args.name, args.all);
             }
             args::Search::Steam { r#mod: _m } => {
                 todo!()
@@ -28,14 +23,9 @@ async fn main() {
         },
 
         args::Commands::SearchLocally {
-            string: m,
-            authors,
-            version,
-            steam_id,
-            name,
-            all,
+            args
         } => {
-            search::search_locally(&path, &m, authors, version, steam_id, name, all);
+            search::search_locally(&path, &args.string, args.authors, args.version, args.steam_id, args.name, args.all);
         }
 
         args::Commands::SearchSteam { r#mod: _m } => {
