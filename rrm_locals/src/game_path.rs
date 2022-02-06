ee9cf275a86caf9e6341466816fb3af229fcf79e
AOx0 <50227494+AOx0@users.noreply.github.com>
2022-02-06 02:33:44 -0600

    Impl Clone for GamePath

--- rrm_locals/src/game_path.rs
+++ rrm_locals/src/game_path.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 use std::path::{Path, PathBuf};
 use std::process::exit;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct GamePath(Box<Path>);
 
 impl GamePath {
