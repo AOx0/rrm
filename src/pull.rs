b766cd72e8348184c61bc080bf486edef328d563
pswsm <53122461+pswsm@users.noreply.github.com>
2025-01-14 22:46:00 +0100

    renmame install command to group bc error

--- src/pull.rs
+++ src/pull.rs
@@ -1,4 +1,4 @@
-use crate::args::{Install, InstallingOptions, Pull};
+use crate::args::{InstallCommandGroup, InstallingOptions, Pull};
 use crate::install::install;
 use crate::utils::Path;
 use crate::utils::*;
@@ -12,41 +12,39 @@ pub async fn pull(args: Pull, i: Installer, ignored: bool) {
         println!("Listing installed ids: ");
     }
 
-    let ids = mods
-        .iter()
-        .filter_map(|mo| {
-            if args.is_debug() {
-                println!(
-                    "Turn of {}",
-                    Path::new(&mo.path).file_name().unwrap().to_str().unwrap()
-                );
-            }
+    let ids = mods.iter().filter_map(|mo| {
+        if args.is_debug() {
+            println!(
+                "Turn of {}",
+                Path::new(&mo.path).file_name().unwrap().to_str().unwrap()
+            );
+        }
 
-            if Path::new(&mo.path)
-                .file_name()
-                .unwrap()
-                .to_str()
-                .unwrap()
-                .to_string()
-                .starts_with('_')
-                && !ignored
-            {
-                if args.is_verbose() {
-                    println!("Ignoring {}", mo.steam_id);
-                }
-                None
-            } else {
-                if args.is_verbose() {
-                    println!("Adding {}", mo.steam_id);
-                }
-                Some(mo.steam_id.clone())
+        if Path::new(&mo.path)
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_string()
+            .starts_with('_')
+            && !ignored
+        {
+            if args.is_verbose() {
+                println!("Ignoring {}", mo.steam_id);
             }
-        });
+            None
+        } else {
+            if args.is_verbose() {
+                println!("Adding {}", mo.steam_id);
+            }
+            Some(mo.steam_id.clone())
+        }
+    });
 
     let to_install: HashSet<String> = HashSet::from_iter(ids);
     let ids: Vec<String> = to_install.iter().cloned().collect();
 
-    let to_install = Install {
+    let to_install = InstallCommandGroup {
         r#mod: ids,
         filter: None,
         author: false,
